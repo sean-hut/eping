96f6c6588b5ad8ece4b418c14f57fcb035c9b003
Sean Hutchings <seanhut@yandex.com>
2020-10-16 20:08:14 -0400

    D add header comments
    Signed-off-by: Sean Hutchings <seanhut@yandex.com>

---
+++ eping.el
@@ -0,0 +1,39 @@
+;;; eping.el --- Ping websites to check internet connectivity
+
+;; Copyright © 2020 Sean Hutchings
+
+;; Author: Sean Hutchings <seanhut@yandex.com>
+
+;; Maintainer: Sean Hutchings <seanhut@yandex.com>
+
+;; Created: 2020-10-16
+
+;; Keywords: comm, processes, terminals, unix
+
+;; License: BSD Zero Clause License (SPDX: 0BSD)
+
+;; License Contents:
+;; Copyright © 2020 Sean Hutchings <seanhut@yandex.com>
+;;
+;; Permission to use, copy, modify, and/or distribute this software
+;; for any purpose with or without fee is hereby granted.
+;;
+;; THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
+;; WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
+;; WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE
+;; AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
+;; CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+;; OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
+;; NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+;; CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+;; Homepage: https://github.com/sean-hut/eping
+
+;;; Change Log: For all notable changes see CHANGELOG.md
+
+;;; Commentary:
+;; Ping websites to check internet connectivity.
+;;
+;; Repository: https://github.com/sean-hut/eping
+
+;;; Code:
